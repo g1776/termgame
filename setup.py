ccd6177194a3945944a87913dc21ebd6dae2d73e
g1776 <gregoryg323@gmail.com>
2022-12-15 14:20:10 -0500

    moved into termgame folder

--- setup.py
+++ setup.py
@@ -10,8 +10,16 @@ setup(
     author="Gregory Glatzer",
     author_email="gregoryg323@gmail.com",
     url="",
-    packages=["termgame"] + ["termgame." + pkg for pkg in find_packages(where="src")],
+    packages=["termgame"] + ["termgame." + pkg for pkg in find_packages(where="termgame/src")],
     package_dir={
-        "termgame": "src",
+        "termgame": "termgame/src",
     },
+    install_requires=[
+        "Colr==0.9.1",
+        "cursor==1.3.5",
+        "numpy==1.22.3",
+        "Pillow==9.3.0",
+        "pymunk==6.4.0",
+        "python-dotenv==0.21.0",
+    ],
 )
