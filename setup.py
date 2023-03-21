20d2e12482a5307c18922c64d542e3306e241f7c
g1776 <gregoryg323@gmail.com>
2023-03-21 13:49:08 -0400

    unneccesary folders

--- setup.py
+++ setup.py
@@ -6,22 +6,22 @@ from pathlib import Path
 
 
 def get_install_requires():
-    with open("./termgame/requirements.txt") as f:
+    with open("./requirements.txt") as f:
         return f.read().splitlines()
 
 
 setup(
     name="termgame",
-    version="0.0.3",
+    version="0.0.4",
     description="Terminal-based graphical game engine",
     long_description=Path("README.md").read_text(encoding="utf-8"),
     long_description_content_type="text/markdown",
     author="Gregory Glatzer",
     author_email="gregoryg323@gmail.com",
     url="",
-    packages=["termgame"] + ["termgame." + pkg for pkg in find_packages(where="termgame/src")],
+    packages=["termgame"] + ["termgame." + pkg for pkg in find_packages(where="src")],
     package_dir={
-        "termgame": "termgame/src",
+        "termgame": "src",
     },
     install_requires=get_install_requires(),
 )
